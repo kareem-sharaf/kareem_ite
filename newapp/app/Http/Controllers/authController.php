1cae76de89de050fe0e9289bc7059718bf75fe62
kareem-sharaf <151177267+kareem-sharaf@users.noreply.github.com>
2023-11-26 12:07:06 +0300

    clean code

--- newapp/app/Http/Controllers/authController.php
+++ newapp/app/Http/Controllers/authController.php
@@ -52,7 +52,38 @@ class authController extends Controller
 
 
 
+   public function register_pharmacy(Request $request)
+   {
 
+   $request->validate([
+        'name'=>'required',
+        'number'=>'required|min:10|unique:users,number',
+        'password'=>'required|min:8',
+        'email'=>'required|min:10|unique:users,email',
+    ]
+    ,['name.required'=>'name is required'],
+        ['number.required'=>'mobile number is required'],
+        ['number.unique'=>'mobile number already taken'],
+        [ 'password.required'=>'password is required'],
+        ['email.required'=>'email is required'],
+        ['email.unique'=>'email already taken'],
+   );
+   $input = $request->all();
+    $input['password'] = Hash::make($input['password']);
+    $input['admin'] = false;
+    $user = User::create($input);
+    $success['token'] = $user->createToken('kareem')->accessToken;
+    $success['name'] = $user->name;
+
+    $message="Registration completed";
+        return response()->json(
+            [
+                'status'=>1,
+                'message'=>$message,
+                'data'=>$success,
+            ]
+        );
+   }
 
 
 
@@ -89,159 +120,11 @@ class authController extends Controller
 
 
 
-   public function warehouse_forget(Request $request)
-{
-    $user = User::where('email', $request->input('email'))
-                ->where('number', $request->input('number'))
-                ->first();
-
-    if($user){
-        $token = $user->createToken('kareem')->accessToken;
-        $message = "check the user";
-        return response()->json([
-            'status' => 1,
-            'message' => $message,
-            'token' => $token,
-        ]);
-    } else {
-        $message = "wrong inputs";
-        return response()->json([
-            'status' => 0,
-            'message' => $message,
-            'token' => null,
-        ]);
-    }
-}
-
-
-
-public function warehouse_reset_password(Request $request)
-{
-    $user = auth()->user();
-
-    $newPassword = $request->input('new_password');
-    $user->password = bcrypt($newPassword);
-    $user->save();
-
-    $token = $user->createToken('kareem')->accessToken;
-        $message = "Login completed";
-        return response()->json([
-            'status' => 1,
-            'message' => $message,
-            'token' => $token,
-        ]);
-    }
-
-
-
-    public function warehouse_edit_info(Request $request){
-        $user = auth()->user();
-
-        if($request->has('name')){
-            $user->name = $request->input('name');
-        }
-
-        if($request->has('password')){
-            $user->password = bcrypt($request->input('password'));
-        }
-
-        if($request->has('number')){
-            $user->number = $request->input('number');
-        }
-
-        $user->save();
-
-        $message = "the information updated successfully";
-        return response()->json([
-            'status' => 1,
-            'message' => $message,
-        ]);
-    }
-
-
-    public function warehouse_delete(Request $request){
-        $user = auth()->user();
-        $user->delete();
-        $message = "The user deleted successfully.";
-         return response()->json([
-        'status' => 1,
-        'message' => $message,
-        'data' => $user,
-    ]);
-
-    }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//functions for pharmacy
-
-public function register_pharmacy(Request $request)
-{
-
-$request->validate([
-     'name'=>'required',
-     'number'=>'required|min:10|unique:users,number',
-     'password'=>'required|min:8',
-     'email'=>'required|min:10|unique:users,email',
- ]
- ,['name.required'=>'name is required'],
-     ['number.required'=>'mobile number is required'],
-     ['number.unique'=>'mobile number already taken'],
-     [ 'password.required'=>'password is required'],
-     ['email.required'=>'email is required'],
-     ['email.unique'=>'email already taken'],
-);
-$input = $request->all();
- $input['password'] = Hash::make($input['password']);
- $input['admin'] = false;
- $user = User::create($input);
- $success['token'] = $user->createToken('kareem')->accessToken;
- $success['name'] = $user->name;
-
- $message="Registration completed";
-     return response()->json(
-         [
-             'status'=>1,
-             'message'=>$message,
-             'data'=>$success,
-         ]
-     );
-}
-
-
-
-
-
-public function login_pharmacy(Request $request)
+   public function login_pharmacy(Request $request)
 {
 
 
@@ -281,6 +164,57 @@ else{
  );
  }
 }
+
+
+
+public function logout(Request $request)
+{
+
+$token= Auth::user()->token();
+$token->revoke();
+
+
+ return response()->json(
+     [
+         'status'=>1,
+         'message'=>'logout success',
+         'data'=>[],
+
+     ]
+ );
+
+}
+
+
+
+   public function warehouse_forget(Request $request)
+{
+    $user = User::where('email', $request->input('email'))
+                ->where('number', $request->input('number'))
+                ->first();
+
+    if($user){
+        $token = $user->createToken('kareem')->accessToken;
+        $message = "check the user";
+        return response()->json([
+            'status' => 1,
+            'message' => $message,
+            'token' => $token,
+        ]);
+    } else {
+        $message = "wrong inputs";
+        return response()->json([
+            'status' => 0,
+            'message' => $message,
+            'token' => null,
+        ]);
+    }
+}
+
+
+
+
+
 public function pharmacy_forget(Request $request)
 {
     $user = User::where('email', $request->input('email'))
@@ -307,7 +241,7 @@ public function pharmacy_forget(Request $request)
 
 
 
-public function pharmacy_reset_password(Request $request)
+public function reset_password(Request $request)
 {
     $user = auth()->user();
 
@@ -325,7 +259,8 @@ public function pharmacy_reset_password(Request $request)
     }
 
 
-    public function pharmacy_edit_info(Request $request){
+
+    public function edit_info(Request $request){
         $user = auth()->user();
 
         if($request->has('name')){
@@ -350,7 +285,10 @@ public function pharmacy_reset_password(Request $request)
     }
 
 
-    public function pharmacy_delete(Request $request){
+
+
+
+    public function delete_the_user(Request $request){
         $user = auth()->user();
         $user->delete();
         $message = "The user deleted successfully.";
@@ -362,35 +300,4 @@ public function pharmacy_reset_password(Request $request)
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-   public function logout(Request $request)
-   {
-
-$token= Auth::user()->token();
-$token->revoke();
-
-
-    return response()->json(
-        [
-            'status'=>1,
-            'message'=>'logout success',
-            'data'=>[],
-
-        ]
-    );
-
-}
-
-
-}
+ }
--- newapp/app/Http/Controllers/productController.php
+++ newapp/app/Http/Controllers/productController.php
@@ -1,17 +1,51 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Auth;
-use Validator;
-use App\Models\Order;
 use App\Models\Product;
-class productController extends Controller
+use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
+class ProductController extends Controller
 {
-    
-    public function store_warehouse(Request $request)
+
+    public function show_all_products_to_warehouse()
+{
+    $user = auth()->user();
+    $id = $user->id;
+
+    $product = Product::where('warehouse_id', $id)->get('name_scientific');
+    $message = "this is the all products";
+
+    return response()->json([
+        'status' => 1,
+        'message' => $message,
+        'data' => $product,
+    ]);
+}
+
+
+
+        public function show_all_products()
     {
+        $product = Product::get('name_scientific');
+        $message = "this is the all products";
+
+        return response()->json([
+            'status' => 1,
+            'message' => $message,
+            'data' => $product,
+        ]);
+    }
+
+
+
+    public function create_products(Request $request)
+    {
+        $user = auth()->user();
+        $id = $user->id;
+        if($user->admin){
         $input = $request->all();
         $validator = Validator::make($input,[
             'name_scientific'=>'required',
@@ -33,6 +67,29 @@ class productController extends Controller
             );
         }
 
+    //اذا كان المنتج موجود سابقا فقط عدل الكمية
+
+        $existingProduct = Product::where('warehouse_id', $id)
+                            ->where('name_scientific', $input['name_scientific'])
+                            ->where('name_trade', $input['name_trade'])
+                            ->where('type', $input['type'])
+                            ->where('company', $input['company'])
+                            ->where('ex_date', $input['ex_date'])
+                            ->where('price', $input['price'])
+                            ->first();
+
+        if ($existingProduct) {
+            $existingProduct->quantity += $input['quantity'];
+            $existingProduct->save();
+            $message = "update product quantity successfully";
+            return response()->json([
+                'status' => 1,
+                'message' => $message,
+                'data' => $existingProduct
+            ]);
+        }
+//اذا لم يكن المنتج موجود اضف منتجا جديدا
+
         $user = Auth::user();
         $input['warehouse_id'] = $user->id;
         $product = Product::create($input);
@@ -45,5 +102,155 @@ class productController extends Controller
             ]
         );
 
+    }else{
+        $message="you can't add products ";
+        return response()->json(
+            [
+                'status'=>0,
+                'message'=>$message
+            ]
+        );
     }
+}
+
+
+    public function search_to_product_for_warehouse($name)
+{
+    $user = auth()->user();
+    $id = $user->id;
+    $product = Product::where('warehouse_id', $id)
+                        ->where('name_scientific', $name)
+                        ->orwhere('type', $name)
+                        ->get(['name_scientific', 'type']);
+
+    if (is_null($product)) {
+        $message = "The product doesn't exist.";
+        return response()->json([
+            'status' => 0,
+            'message' => $message,
+        ]);
+    }
+
+    $message = "This is the product.";
+    return response()->json([
+        'status' => 1,
+        'message' => $message,
+        'data' => $product,
+    ]);
+}
+
+public function search_to_product_for_pharmacy($name)
+{
+    $product = Product::where('name_scientific', $name)
+                        ->orwhere('type', $name)
+                        ->get(['name_scientific', 'type']);
+
+    if (is_null($product)) {
+        $message = "The product doesn't exist.";
+        return response()->json([
+            'status' => 0,
+            'message' => $message,
+        ]);
+    }
+
+    $message = "This is the product.";
+    return response()->json([
+        'status' => 1,
+        'message' => $message,
+        'data' => $product,
+    ]);
+}
+
+
+public function show_all_warehouses()
+    {
+        $warehouse = User::where('admin', true)->get('name');
+        $message = "this is the all warehouses";
+
+        return response()->json([
+            'status' => 1,
+            'message' => $message,
+            'data' => $warehouse,
+        ]);
+    }
+
+
+public function edit_product(Request $request,$product_id)
+{
+        $user = auth()->user();
+        $id = $user->id;
+        $product = Product::where('warehouse_id', $id)->find($product_id);
+    if ($user->id !== $product['warehouse_id']) {
+        $message = "You are not authorized to update this product.";
+        return response()->json([
+            'status' => 0,
+            'message' => $message,
+        ]);
+    }
+
+    $input = $request->all();
+    $validator = Validator::make($input, [
+        'name_scientific' => 'required',
+        'name_trade' => 'required',
+        'type' => 'required',
+        'company' => 'required',
+        'quantity' => 'required',
+        'ex_date' => 'required',
+        'price' => 'required'
+    ]);
+
+    if ($validator->fails()) {
+        $message = "There is an error in the inputs.";
+        return response()->json([
+            'status' => 0,
+            'message' => $message,
+            'data' => $input,
+        ]);
+    }
+
+    $product->name_scientific = $input['name_scientific'];
+    $product->name_trade = $input['name_trade'];
+    $product->type = $input['type'];
+    $product->company = $input['company'];
+    $product->quantity = $input['quantity'];
+    $product->ex_date = $input['ex_date'];
+    $product->price = $input['price'];
+    $product->save();
+
+    $message = "The product has been updated successfully.";
+    return response()->json([
+        'status' => 1,
+        'message' => $message,
+        'data' => $product
+    ]);
+}
+
+
+
+    public function delete_product($id_product)
+    {
+        //delete the products which the warehouse want deleted it.
+        $user = auth()->user();
+        $id = $user->id;
+        $product = Product::where('warehouse_id', $id)->find($id_product);
+        if (is_null($product)) {
+            $message = "The product doesn't exist.";
+            return response()->json([
+                'status' => 0,
+                'message' => $message,
+            ]);
+        }
+        $product->delete();
+        $message = "The product deleted successfully.";
+         return response()->json([
+        'status' => 1,
+        'message' => $message,
+        'data' => $product,
+    ]);
+
+    }
+
+
+
+
 }
