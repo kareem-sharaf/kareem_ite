c666a4d09e7d94b92e18e3b597565fa1a6339701
kareem-sharaf <151177267+kareem-sharaf@users.noreply.github.com>
2023-11-26 12:50:11 +0300

    edit api

--- newapp/routes/api.php
+++ newapp/routes/api.php
@@ -17,12 +17,11 @@ Route::post('/info',function(request $request)
 
 
 //Routes for order.
-Route::get('/warehouse/all_orders_in_warehouse',[App\Http\Controllers\orderController::class,'all_orders_in_warehouse']);
-Route::post('/warehouse/store_warehouse',[App\Http\Controllers\productController::class,'store_warehouse']);
-Route::post('/warehouse/edit_status',[App\Http\Controllers\orderController::class,'edit_status']);
 Route::middleware('auth:api')->group( function () {
 Route::get('/pharmacy/all_orders_in_pharmacy',[App\Http\Controllers\orderController::class,'all_orders_in_pharmacy']);
 Route::post('/pharmacy/create_order',[App\Http\Controllers\orderController::class,'create_order']);
+Route::get('/warehouse/all_orders_in_warehouse',[App\Http\Controllers\orderController::class,'all_orders_in_warehouse']);
+Route::post('/warehouse/edit_status',[App\Http\Controllers\orderController::class,'edit_status']);
 });
 //*********
 
@@ -61,21 +60,13 @@ Route::delete('/delete_user',[App\Http\Controllers\authController::class,'delete
 
 //Routes for products.
 Route::middleware('auth:api')->group( function () {
-//Routes for products_warehouse
-Route::get('/warehouse/index',[App\Http\Controllers\WarehouseProductController::class,'index_warehouse']);
-Route::post('/warehouse/store',[App\Http\Controllers\WarehouseProductController::class,'store_warehouse']);
-Route::get('/warehouse/show/{name}',[App\Http\Controllers\WarehouseProductController::class,'show_warehouse']);
-Route::put('/warehouse/update/{id}',[App\Http\Controllers\WarehouseProductController::class,'update_warehouse']);
-Route::delete('/warehouse/destroy/{id}',[App\Http\Controllers\WarehouseProductController::class,'destroy_warehouse']);
-//****************
-Route::get('/warehouse/all_orders_in_warehouse',[App\Http\Controllers\orderController::class,'all_orders_in_warehouse']);
-Route::post('/warehouse/store_warehouse',[App\Http\Controllers\productController::class,'store_warehouse']);
-Route::post('/warehouse/edit_status',[App\Http\Controllers\orderController::class,'edit_status']);
+Route::get('/warehouse/all_products',[App\Http\Controllers\ProductController::class,'show_all_products_to_warehouse']);
+Route::post('/create_product',[App\Http\Controllers\ProductController::class,'create_products']);
+Route::get('/warehouse/searching/{name}',[App\Http\Controllers\ProductController::class,'search_to_product_for_warehouse']);
+Route::put('/edit_product/{id}',[App\Http\Controllers\ProductController::class,'edit_product']);
+Route::delete('/delete_product/{id}',[App\Http\Controllers\ProductController::class,'delete_product']);
 });
-
-//Routes for products_pharmacy
-Route::get('/pharmacy/index',[App\Http\Controllers\PharmacyProductController::class,'index_pharmacy']);
-Route::get('/pharmacy/index/{id}',[App\Http\Controllers\PharmacyProductController::class,'special_index_pharmacy']);
-Route::get('/pharmacy/show/{name}',[App\Http\Controllers\PharmacyProductController::class,'show_pharmacy']);
-//Routes for warehouser_pharmacy
-Route::get('/all_warehouses',[App\Http\Controllers\PharmacyWarehouseController::class,'index']);
+Route::get('/all_products',[App\Http\Controllers\ProductController::class,'show_all_products']);
+Route::get('/pharmacy/searching/{name}',[App\Http\Controllers\ProductController::class,'search_to_product_for_pharmacy']);
+Route::get('/show_all_warehouses',[App\Http\Controllers\ProductController::class,'show_all_warehouses']);
+//****************
